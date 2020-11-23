94f21b1740da40f6ef8f30d70ca21d55e18f2946
sahil-1602 <sahildarji16@gmail.com>
2020-11-23 17:25:47 +0530

    Initial Commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/8b/a02db337822d001b1858fc96af340cfd
@@ -0,0 +1,58 @@
+package oo_car_info;
+
+public class Car_accessories {
+    private double istereo = 12500;
+    private double modified = 10450;
+    private double customized = 9000;
+    private double leather = 4000;
+    private double GPS = 4999;
+
+    public double getIstereo() {
+        return istereo;
+    }
+
+    public void setIstereo(double istereo) {
+        this.istereo = istereo;
+    }
+
+    public double getModified() {
+        return modified;
+    }
+
+    public void setModified(double modified) {
+        this.modified = modified;
+    }
+
+    public double getCustomized() {
+        return customized;
+    }
+
+    public void setCustomized(double customized) {
+        this.customized = customized;
+    }
+
+    public double getLeather() {
+        return leather;
+    }
+
+    public void setLeather(double leather) {
+        this.leather = leather;
+    }
+
+    public double getGPS() {
+        return GPS;
+    }
+
+    public void setGPS(double GPS) {
+        this.GPS = GPS;
+    }
+    public double accessorycost;
+    public double GetAmount()
+    {
+        accessorycost = istereo + modified +customized + leather + GPS;
+        return accessorycost;
+    }
+    
+    
+}
+
---
+++ Car Management System/src/oo_car_info/Customer.java
@@ -0,0 +1,91 @@
+package oo_car_info;
+
+public class Customer {
+    //Data Member
+    private String CustomerId;
+    private String firstName;
+    private String Surname;
+    private String address;
+    private String postcode;
+    private String State;
+    private String City;
+    private Double Downpayment;
+    private Double Deposit;
+
+    public String getCustomerId() {
+        return CustomerId;
+    }
+
+    public void setCustomerId(String CustomerId) {
+        this.CustomerId = CustomerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return Surname;
+    }
+
+    public void setSurname(String Surname) {
+        this.Surname = Surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public Double getDownpayment() {
+        return Downpayment;
+    }
+
+    public void setDownpayment(Double Downpayment) {
+        this.Downpayment = Downpayment;
+    }
+
+    public Double getDeposit() {
+        return Deposit;
+    }
+
+    public void setDeposit(Double Deposit) {
+        this.Deposit = Deposit;
+    }
+    
+    
+    
+    
+}
+
